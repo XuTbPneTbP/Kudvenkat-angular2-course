45036cedadd45fefaa0653b0e0dede643a559ce6
XuTbPneTbP <exceptionhelper@gmail.com>
2017-11-03 21:16:22 +0200

    included heroes demo

--- angular2/src/app/app.module.ts
+++ angular2/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeListComponent } from './employee/employee-list/employeeList.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    EmployeeComponent
+    EmployeeComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule
